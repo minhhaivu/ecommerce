65bb0b655e6a33a50edf57f236e38775d35f5324
Hai <minhhai.vu83@gmail.com>
2022-05-18 22:55:30 +0700

    Refactor Quick Add to Cart Test

--- src/test/java/pages/AbstractPage.java
+++ src/test/java/pages/AbstractPage.java
@@ -2,13 +2,20 @@ package pages;
 
 import driver.DriverGetter;
 import lombok.Getter;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 @Getter
 public abstract class AbstractPage {
 
     protected WebDriver pageDriver = DriverGetter.getDriver();
+
+    protected long TIME_OUT_IN_SECOND = 10000;
 
     public AbstractPage() {
         PageFactory.initElements(pageDriver, this);
@@ -16,5 +23,18 @@ public abstract class AbstractPage {
 
     public void close() {
         pageDriver.quit();
+    }
+
+    public void hoverMouse(WebElement element) {
+        scrollInToView(element);
+        Actions action = new Actions(pageDriver);
+        action.moveToElement(element).perform();
+        new WebDriverWait(pageDriver, TIME_OUT_IN_SECOND)
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void scrollInToView(WebElement element) {
+        JavascriptExecutor js = (JavascriptExecutor) pageDriver;
+        js.executeScript("arguments[0].scrollIntoView();", element);
     }
 }
